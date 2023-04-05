4f25def04288d86f0f24d7c5be6f3eff09c59bcc
Damien Plénard <damien@plenard.me>
2023-04-05 11:37:55 +0200

    add output for cluster-0

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "cluster_zero_kubeconfig" {
+  value = module.cluster_zero.kubeconfig
+  sensitive = true
+}
+
+output "cluster_zero_talosconfig" {
+  value = module.cluster_zero.talosconfig
+  sensitive = true
+}
